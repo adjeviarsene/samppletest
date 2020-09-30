159e42152cb19d7d0046124f8b0a6a58e45249f3
A <cybdry@gmail.com>
2020-09-30 23:40:58 +0300

    Commit again

--- backend.js
+++ backend.js
@@ -1,5 +1,15 @@
 function Converto(decimal,binaire,hexaddecimal){
     this.decimal=decimal;
     this.binaire=binaire;
-    this.hexaddecimal=hexaddecimal
+    this.hexaddecimal=hexaddecimal;
 }
+const number=[];
+
+for(var i=0;i<16;i++){
+    number =new Converto(
+        i,
+        i.toString(2),
+        i.toString(16).toUpperCase()
+    );
+}
+console.table(number);
